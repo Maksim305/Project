64ab73a51736c2b6ef81e61802bd0e1769d2c896
Maksim <maksim.gadajcuk5@gmail.com>
2020-05-31 14:43:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//alert('Ваш компьютер будет заражен вирусами!');
+
+//const result = confirm("Ваш компьютер будет заражен вирусами!");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?");
+//console.log(typeof(answer));
+
+//const anwers = [];
+
+//answers[0] = prompt('Как дела?', '');
+//answers[1] = prompt('Как ваше имя?', '');
+//answers[2] = prompt('Сколько лет?', '');
+
+//console.log(typeof (answers));
+//console.log(typeof(null));
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Maksim";
+//alert(`Привет, ${user}`);
+
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+++incr;
+--decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+const isCheked = false,
+    isClose = false;
+
+console.log(isCheked || !isClose);
